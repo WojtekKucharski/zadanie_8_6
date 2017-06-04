6066a9efc3c02a4b59879c017adecc55db42a76a
WojtekKucharski <w.kucharski7@gmail.com>
2017-06-04 23:45:41 +0200

    changed part of code with "if"

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,12 @@ var b =  prompt('podaj wartość b');
 var equalingResult = (a * a) + (2 * a * b) - (b * b);
 
 if (equalingResult == 0) {
-alert ('wynik równania dla a = ' + a + ' i b = ' + b + ' jest równy 0');
-console.log('wynik równania dla a: ' + a + ' i b: ' + b + 'jest równy 0');
-}
-
-if (equalingResult > 0) { 
-alert ('wynik równania jest dodatni');
-console.log ('wynik równania jest dodatni');
-}
-
-if (equalingResult < 0) { 
-alert ('wynik równania jest ujemny');
-console.log ('wynik równania jest ujemny');
+	alert ('wynik równania dla a = ' + a + ' i b = ' + b + ' jest równy 0');
+	console.log('wynik równania dla a: ' + a + ' i b: ' + b + 'jest równy 0');
+} else if (equalingResult > 0) { 
+	alert ('wynik równania jest dodatni');
+	console.log ('wynik równania jest dodatni');
+} else { 
+	alert ('wynik równania jest ujemny');
+	console.log ('wynik równania jest ujemny');
 }
